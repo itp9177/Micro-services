85974b31b36fec7a61dc757a8a63925ecea6a343
itp9177 <indutharanga@gmail.com>
2024-02-04 14:12:25 +0530

    inventory service src folder

---
+++ inventory-service/src/main/java/com/itp/inventory/InventoryService.java
@@ -0,0 +1,14 @@
+package com.itp.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryService {
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryService.class, args);
+
+    }
+
+
+}
