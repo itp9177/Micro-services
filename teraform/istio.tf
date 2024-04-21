d91ed4281ba35c72cfea29da196859801c3b2cd1
itp9177 <indutharanga@gmail.com>
2024-04-21 20:35:33 +0530

    terraform code fix

--- teraform/istio.tf
+++ teraform/istio.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "istio-base" {
   name  = "istio-base"
-  namespace = istio-system
+  namespace = "istio-system"
   create_namespace = true
   chart = "./charts/base-1.21.0"
 
@@ -12,14 +12,14 @@ resource "helm_release" "istio-base" {
 
 resource "helm_release" "istiod" {
   name  = "istiod"
-  namespace = istio-system
+  namespace = "istio-system"
   chart = "./charts/istiod-1.21.0.tgz"
 
 }
 #kiali for istio
 resource "helm_release" "kiali-server" {
   name  = "kiali-server"
-  namespace = istio-system
+  namespace = "istio-system"
   chart = "./charts/kiali-server-1.82.0.tgz"
 
   set {
