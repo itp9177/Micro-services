a739fb4c4a607bfc14e904e3504f7b709742ff49
itp9177 <indutharanga@gmail.com>
2024-04-22 22:42:01 +0530

    kafka strimzi helm terraform script

--- teraform/kafka_shamzi.tf
+++ teraform/kafka_shamzi.tf
@@ -1,6 +1,17 @@
 
-resource "helm_release" "kfka_shamzi" {
-  name  = "shamzi"
-  chart = ""
+resource "helm_release" "strimzi-cluster-operator" {
+  name  = "strimzi-cluster-operator"
+  chart =  "./charts/strimzi-kafka-operator-0.40.0.tgz"
+  
+  set {
+    name  = "replicas"
+    value = "2"
+  }
 }
 
+#kafka cluster 
+resource "kubernetes_manifest" "kafkacluster" {
+  #provider = kubernetes-alpha
+
+  manifest = yamldecode(file("./maifests/kafka/kafkacluster.yml"))
+}
