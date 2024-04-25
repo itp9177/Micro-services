812ee84f831536938762b163fc6cc75a3cc1a65f
itp9177 <indutharanga@gmail.com>
2024-04-25 23:33:45 +0530

    code fix

---
+++ flink-elasticsearch-service/src/main/java/com/itp/analytics/kafka/KafkaProducer.java
@@ -0,0 +1,39 @@
+package com.itp.analytics.kafka;
+
+import com.itp.openapi.model.V1ForecastGet200Response;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducer {
+
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    public String producerServer;
+
+    @Bean(name = "kafka1")
+    public KafkaTemplate<String, V1ForecastGet200Response> kafkaTemplate(ProducerFactory<String, V1ForecastGet200Response> producerFactoryJson)
+    {
+        return new KafkaTemplate<>(producerFactoryJson);
+    }
+
+    @Bean
+    public ProducerFactory<String, V1ForecastGet200Response> producerFactoryJson() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,producerServer);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+}
---
+++ flink-elasticsearch-service/src/main/java/com/itp/analytics/kafka/Topic.java
@@ -0,0 +1,37 @@
+package com.itp.analytics.kafka;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class Topic {
+    @Bean
+    public KafkaAdmin admin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094");
+        return new KafkaAdmin(configs);
+    }
+    @Bean
+    public NewTopic topic1() {
+        return TopicBuilder.name("wea2")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+    @Bean
+    public NewTopic topic2() {
+        return TopicBuilder.name("wea4")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+}
